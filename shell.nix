74797964367330b8c40f0f6246880507467d9637
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2019-07-30 12:25:01 +0200

    Sketch nix-shell file

---
+++ shell.nix
@@ -0,0 +1,34 @@
+{ nixpkgs ? (import <nixpkgs> {}).fetchFromGitHub {
+    owner  = "NixOS";
+    repo   = "nixpkgs";
+    rev    = "5ac6ab091a4883385e68571425fb7fef4d74c207";
+    sha256 = "0rksyhnnj5028n2ql3jkf98vpd8cs1qf6rckgvx9jq2zf1xqsbla";
+  }}:
+
+with import nixpkgs {};
+
+let
+  # For the documentation tools we use an older Nixpkgs since the
+  # newer versions seem to be not working great...
+  old-nixpkgs-src = fetchFromGitHub {
+                      owner  = "NixOS";
+                      repo   = "nixpkgs";
+                      rev    = "d0d905668c010b65795b57afdf7f0360aac6245b";
+                      sha256 = "1kqxfmsik1s1jsmim20n5l4kq6wq8743h5h17igfxxbbwwqry88l";
+                    };
+  old-nixpkgs     = import old-nixpkgs-src {};
+  docs            = import ./nix/docs.nix { nixpkgs = old-nixpkgs-src; };
+
+in
+stdenv.mkDerivation rec {
+  name = "zug-env";
+  buildInputs = [
+    cmake
+  ];
+  shellHook = ''
+    export ZUG_ROOT=`dirname ${toString ./shell.nix}`
+    addToSearchPath PATH "$ZUG_ROOT/build"
+    addToSearchPath PATH "$ZUG_ROOT/build/example"
+    addToSearchPath PATH "$ZUG_ROOT/build/test"
+  '';
+}
