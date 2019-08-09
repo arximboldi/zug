cc2f59990c6a13b729b6f8c28755fe6421d945f9
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2019-08-09 16:30:04 +0200

    Add drop, drop_while, each and eager transducers

---
+++ zug/transducer/drop.hpp
@@ -0,0 +1,35 @@
+//
+// zug: transducers for C++
+// Copyright (C) 2019 Juan Pedro Bolivar Puente
+//
+// This software is distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+//
+
+#pragma once
+
+#include <zug/skip.hpp>
+#include <zug/state_wrapper.hpp>
+
+namespace zug {
+
+/*!
+ * Similar to clojure.core/drop$1
+ */
+constexpr auto drop = [](auto n) {
+    return [=](auto step) mutable {
+        return [=](auto&& s, auto&&... is) mutable {
+            auto count = state_data(ZUG_FWD(s), [&] { return n; });
+            return count != 0 ? wrap_state(skip(step,
+                                                state_unwrap(ZUG_FWD(s)),
+                                                ZUG_FWD(is)...),
+                                           count - 1)
+                              : wrap_state(call(step,
+                                                state_unwrap(ZUG_FWD(s)),
+                                                ZUG_FWD(is)...),
+                                           count);
+        };
+    };
+};
+
+} // namespace zug
---
+++ zug/transducer/each.hpp
@@ -0,0 +1,29 @@
+//
+// zug: transducers for C++
+// Copyright (C) 2019 Juan Pedro Bolivar Puente
+//
+// This software is distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+//
+
+#pragma once
+
+#include <zug/compat/invoke.hpp>
+#include <zug/util.hpp>
+
+namespace zug {
+
+/*!
+ * Transducer that evaluates `action` on each input, forwarding the
+ * input down the original inputs down the transducer chain.
+ */
+constexpr auto each = [](auto action) {
+    return [=](auto step) mutable {
+        return [=](auto&& s, auto&&... is) mutable {
+            invoke(action, is...);
+            return step(ZUG_FWD(s), ZUG_FWD(is)...);
+        };
+    };
+};
+
+} // namespace zug
