a418709c51958a2b2a99acfc831867a774400bd3
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2019-09-05 13:44:10 +0200

    Add take_nth transducer

---
+++ test/transducer/take_nth.cpp
@@ -0,0 +1,46 @@
+//
+// zug: transducers for C++
+// Copyright (C) 2019 Juan Pedro Bolivar Puente
+//
+// This software is distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+//
+
+#include <catch2/catch.hpp>
+
+#include <zug/into_vector.hpp>
+#include <zug/transducer/take.hpp>
+#include <zug/transducer/take_nth.hpp>
+
+#include "../spies.hpp"
+
+using namespace zug;
+
+TEST_CASE("take_nth, into")
+{
+    using namespace std::placeholders;
+    auto v = std::vector<int>{1, 2, 3, 4, 5};
+
+    {
+        auto res = into(std::vector<int>{}, take_nth(1), v);
+        CHECK(res == (decltype(res){1, 2, 3, 4, 5}));
+    }
+
+    {
+        auto res = into(std::vector<int>{}, take_nth(2), v);
+        CHECK(res == (decltype(res){1, 3, 5}));
+    }
+
+    {
+        auto res = into(std::vector<int>{}, take_nth(3), v);
+        CHECK(res == (decltype(res){1, 4}));
+    }
+}
+
+TEST_CASE("take_nth, compose")
+{
+    using namespace std::placeholders;
+    auto v   = std::vector<int>{1, 2, 3, 4, 5};
+    auto res = into_vector(comp(take_nth(2), take(2)), v);
+    CHECK(res == (decltype(res){1, 3}));
+}
---
+++ zug/transducer/take_nth.hpp
@@ -0,0 +1,40 @@
+//
+// zug: transducers for C++
+// Copyright (C) 2019 Juan Pedro Bolivar Puente
+//
+// This software is distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+//
+
+#pragma once
+
+#include <zug/state_wrapper.hpp>
+#include <zug/with_state.hpp>
+
+namespace zug {
+
+/*!
+ * Similar to clojure.core/take_nth$1
+ */
+auto take_nth = [](auto nth) {
+    return [=](auto step) {
+        return [=](auto&& s, auto&&... is) mutable {
+            return with_state(
+                ZUG_FWD(s),
+                [&](auto&& st) {
+                    return wrap_state(step(ZUG_FWD(st), ZUG_FWD(is)...), 1);
+                },
+                [&](auto&& st) {
+                    auto count = state_wrapper_data(ZUG_FWD(st));
+                    return count != nth
+                               ? wrap_state(state_unwrap(ZUG_FWD(st)),
+                                            count + 1)
+                               : wrap_state(step(state_unwrap(ZUG_FWD(st)),
+                                                 ZUG_FWD(is)...),
+                                            1);
+                });
+        };
+    };
+};
+
+} // namespace zug
