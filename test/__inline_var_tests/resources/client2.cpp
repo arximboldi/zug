f859274843b6a2e720d4422fa8c0f5c75ee2ff0c
Carl Bussey <carl.bussey@native-instruments.com>
2019-09-10 12:12:03 +0200

    inline_var: provide utility to support inline vars pre-C++17
    * slightly refactored tests to move into subfolder
    * required to test inline_var - needs multiple TUs, which test/tests/CMakeLists.txt doesn't support

---
+++ test/__inline_var_tests/resources/client2.cpp
@@ -0,0 +1,24 @@
+//
+// zug: transducers for C++
+// Copyright (C) 2019 Juan Pedro Bolivar Puente (and Carl Bussey, maybe?)
+//
+// This software is distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+//
+
+#include "client2.hpp"
+
+#include <memory>
+
+namespace zug {
+namespace detail {
+namespace client2 {
+
+const to_be_inlined* address_of_inline_var()
+{
+    return std::addressof(inline_var);
+}
+
+} // namespace client2
+} // namespace detail
+} // namespace zug
