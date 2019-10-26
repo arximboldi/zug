46b780ed7850a3d05a1d79093f678bab4e5d6cc8
Carl Bussey <carl.bussey@native-instruments.com>
2019-10-26 17:01:27 +0200

    util: flatten composed
    - avoid nesting composed<composed<composed<composed<composed<T>>>>>

--- zug/util.hpp
+++ zug/util.hpp
@@ -10,6 +10,7 @@
 
 #include <zug/compat/invoke.hpp>
 #include <zug/detail/inline_constexpr.hpp>
+#include <zug/meta/dispatch.hpp>
 
 #include <tuple>
 #include <type_traits>
@@ -89,28 +90,87 @@ constexpr decltype(auto) invoke_composition(Fns&& fns, Args&&... args)
 }
 
 template <typename Fn, typename... Fns>
-struct composed
+struct composed : std::tuple<Fn, Fns...>
 {
-    std::tuple<Fn, Fns...> fns;
+    using base_t = std::tuple<Fn, Fns...>;
+
+    template <
+        typename TupleFns,
+        std::enable_if_t<
+            !std::is_same<composed, std::decay_t<TupleFns>>::value>* = nullptr>
+    composed(TupleFns&& fns)
+        : base_t{std::forward<TupleFns>(fns)}
+    {}
 
     template <typename... T>
     constexpr decltype(auto) operator()(T&&... xs) &
     {
-        return invoke_composition(fns, std::forward<T>(xs)...);
+        return invoke_composition(as_tuple(), std::forward<T>(xs)...);
     }
 
     template <typename... T>
     constexpr decltype(auto) operator()(T&&... xs) const&
     {
-        return invoke_composition(fns, std::forward<T>(xs)...);
+        return invoke_composition(as_tuple(), std::forward<T>(xs)...);
     }
 
     template <typename... T>
     constexpr decltype(auto) operator()(T&&... xs) &&
     {
-        return invoke_composition(std::move(fns), std::forward<T>(xs)...);
+        return invoke_composition(std::move(*this).as_tuple(),
+                                  std::forward<T>(xs)...);
     }
+
+    base_t& as_tuple() & { return *this; }
+
+    const base_t& as_tuple() const& { return *this; }
+
+    base_t&& as_tuple() && { return std::move(*this); }
 };
+
+template <typename T>
+constexpr bool is_composed_v = false;
+
+template <typename... Fns>
+constexpr bool is_composed_v<composed<Fns...>> = true;
+
+template <typename Composed,
+          std::enable_if_t<is_composed_v<Composed>>* = nullptr>
+decltype(auto) to_function_tuple(Composed&& c, meta::try_t)
+{
+    return std::forward<Composed>(c).as_tuple();
+}
+
+template <typename Fn>
+auto to_function_tuple(Fn&& fn, meta::catch_t)
+{
+    return std::make_tuple(std::forward<Fn>(fn));
+}
+
+template <typename T>
+decltype(auto) to_function_tuple(T&& t)
+{
+    return to_function_tuple(std::forward<T>(t), meta::try_t{});
+}
+
+template <typename T>
+struct make_composed_result;
+
+template <typename... Fns>
+struct make_composed_result<std::tuple<Fns...>>
+{
+    using type = composed<Fns...>;
+};
+
+template <typename T>
+using make_composed_result_t = typename make_composed_result<T>::type;
+
+template <typename TupleFns>
+auto make_composed(TupleFns&& fns)
+    -> make_composed_result_t<std::decay_t<TupleFns>>
+{
+    return {std::forward<TupleFns>(fns)};
+}
 
 } // namespace detail
 
@@ -132,9 +192,10 @@ auto comp(F&& f) -> F&&
 
 template <typename Fn, typename... Fns>
 auto comp(Fn&& f, Fns&&... fns)
-    -> detail::composed<std::decay_t<Fn>, std::decay_t<Fns>...>
 {
-    return {std::make_tuple(std::forward<Fn>(f), std::forward<Fns>(fns)...)};
+    return detail::make_composed(
+        std::tuple_cat(detail::to_function_tuple(std::forward<Fn>(f)),
+                       detail::to_function_tuple(std::forward<Fns>(fns))...));
 }
 
 /*!
