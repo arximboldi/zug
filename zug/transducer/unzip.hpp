424b4b00d0f17ef5f80f8792d96e511af37b7f0b
Asa Hammond <asahammond@gmail.com>
2023-02-28 17:55:27 -0800

    unzip needs pragma

--- zug/transducer/unzip.hpp
+++ zug/transducer/unzip.hpp
@@ -5,6 +5,7 @@
 // This software is distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
 //
+#pragma once
 
 #include <zug/compat/apply.hpp>
 #include <zug/compose.hpp>
